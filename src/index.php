4e4702e1c1cbbffafacdab21ed66070dbefa621d
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-08-10 13:27:05 +0900

    Add Docker Compose configuration and initial PHP sample page

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="ja">';
+echo '<head>';
+echo '    <meta charset="UTF-8">';
+echo '    <title>PHP サンプルページ</title>';
+echo '</head>';
+echo '<body>';
+echo '    <h1>ようこそ！PHPサンプルページです</h1>';
+echo '    <p>これはDocker環境で動作するPHPのサンプルページです。</p>';
+echo '    <p>現在時刻: ' . date('Y-m-d H:i:s') . '</p>';
+echo '</body>';
+echo '</html>';
+?>
